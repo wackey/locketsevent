bceca7b512eea9d317aa821e32414121c9ebe7c9
脇村 隆 <wakimura@plus-movement.jp>
2019-05-22 12:24:41 +0900

    ファーストコミット

---
+++ locketsevent.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Lockets event
+Plugin URI: https://lockets.jp/plugin/
+Description: ブログ投稿記事にイベント情報（日時・場所など）を登録や運用を支援するプラグインです
+Version: 1.0.0
+Author:wakcey
+Author URI: https://musilog.net
+License: GPL2
+*/
+?>
